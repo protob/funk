0abdcfae6ac93182dcd253cc6edac6b253542f8b
protob <protobx@gmail.com>
2024-12-18 06:25:20 +0100

    Add configs

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="m-auto h-full w-full">
+    <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+#__nuxt {
+  width: 100%;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  .app-wrap.min-h-screen
+    h1 home
+  </template>
+  
+  <script setup lang="ts">
+  </script>
